e4d57f21f778fc4a93aefbefbc59752a925e7d41
sesar <54498315+sesar2@users.noreply.github.com>
2024-11-30 14:32:06 +0100

    code clean up

--- script.js
+++ script.js
@@ -1,84 +1,81 @@
-import {questions} from '/questions.js'
-
+import { questions } from "/questions.js";
 
 let questionIndex = 0;
 let correctAnswers = 0;
 
-
-const container = document.getElementById('quiz-window');
-const options = document.getElementById('options');
-const nextQuestion = document.getElementById('nxt');
-const question = document.getElementById('question')
-
+const container = document.getElementById("quiz-window");
+const options = document.getElementById("options");
+const nextQuestion = document.getElementById("nxt");
+const question = document.getElementById("question");
 
 
+// funktion för att renderar svarsalternativ
+const renderOptions = (currentQuestion) => {
+    currentQuestion.options.forEach((answer) => {
+    const option = document.createElement("div");
 
+    option.innerHTML = answer.text;
+    option.classList.add("option");
 
-const renderQuestion= () => {
-    
-    options.innerHTML = '';
-    
-    const currentQuestion = questions[questionIndex]
-    question.innerHTML = currentQuestion.question;
-    nextQuestion.classList.add('inactive')
+    // Kollar om isCorrect är true eller false och lägger till en class beroende på vad som stämmer
+    option.addEventListener("click", () => {
+      if (answer.isCorrect) {
+        correctAnswers++;
+        option.classList.add("correct");
+      } else {
+        option.classList.add("incorrect");
+      }
+      // Gör så att "Next"-knappen går att trycka på samt tar bort möjligheten att klicka på resterande alternativ
+      nextQuestion.classList.remove("inactive");
+      Array.from(options.children).forEach(
+        (opt) => (opt.style.pointerEvents = "none")
+      );
+    });
 
-    
-    if(currentQuestion.type !== "checkboxes"){
-        currentQuestion.options.forEach((answer) => {
+    options.appendChild(option);
+  });
+};
+// funktion för att rendera svaralternativ med "checkboxes"
+const renderCheckboxOptions = (currentQuestion) => {  
+    currentQuestion.options.forEach((answer) => {
+      const checkboxOption = document.createElement("div");
+      checkboxOption.classList.add("option");
+      checkboxOption.classList.add("checkbox-option");
+  
+      const checkbox = document.createElement("input");
+      checkbox.type = "checkbox";
+  
+      const label = document.createElement("label");
+      label.htmlFor = "checkbox";
+      label.innerHTML = answer.text;
+  
+      checkboxOption.appendChild(checkbox);
+      checkboxOption.appendChild(label);
+      options.appendChild(checkboxOption);
+    });
+  };
 
-            const option = document.createElement("div");
+const renderQuestion = () => {
+  options.innerHTML = "";
 
-            option.innerHTML = answer.text;
-            option.classList.add("option");
-            
-            // Kollar om isCorrect är true eller false och lägger till en class beroende på vad som stämmer
-            option.addEventListener('click', () => {
-                if (answer.isCorrect) {
-                    correctAnswers++;
-                    option.classList.add("correct");
-                } else {
-                    option.classList.add("incorrect");
-                }
-                // Gör så att "Next"-knappen går att trycka på samt tar bort möjligheten att klicka på resterande alternativ
-                nextQuestion.classList.remove('inactive')
-                Array.from(options.children).forEach(opt => opt.style.pointerEvents = 'none');
-            });
-            
-            options.appendChild(option);
-        });
-    }else{
-        let selectedCorrectAnswers = 0;
-        let totalCorrectAnswers = currentQuestion.options.filter(option => option.isCorrect).length;
+  const currentQuestion = questions[questionIndex];
+  question.innerHTML = currentQuestion.question;
+  nextQuestion.classList.add("inactive");
+// kollar om frågan har typen "checkboxes"
+  if (currentQuestion.type !== "checkboxes") {
+    renderOptions(currentQuestion);
+  } else {
+    renderCheckboxOptions(currentQuestion);
+  }
 
-        currentQuestion.options.forEach(answer => {
-            const checkboxOption = document.createElement('div');
-            checkboxOption.classList.add('option')
-            checkboxOption.classList.add('checkbox-option')
+};
 
-            const checkbox = document.createElement('input')
-            checkbox.type = "checkbox"
-            
-            const label = document.createElement('label')
-            label.htmlFor = 'checkbox'
-            label.innerHTML = answer.text
+const handleClick = () => {
+  questionIndex++;
+  renderQuestion();
+  console.log(correctAnswers);
+};
 
-            checkboxOption.appendChild(checkbox)
-            checkboxOption.appendChild(label)
-            options.appendChild(checkboxOption)
-        });
-    }
-    }
-    
-    
-    const handleClick = () => {
-        questionIndex++;
-        renderQuestion()
-        console.log(correctAnswers)
-    }
-    
+nextQuestion.addEventListener("click", handleClick);
 
-
-
-nextQuestion.addEventListener('click', handleClick);
-
-renderQuestion()
+renderQuestion();
