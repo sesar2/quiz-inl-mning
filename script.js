5102c4fed96eb708939422d006953728b3374967
sesar <54498315+sesar2@users.noreply.github.com>
2024-11-30 13:50:08 +0100

    logic to choose answer "multiple choice" and "true or false" works. TODO: write logic for checkbox questions

--- script.js
+++ script.js
@@ -1,7 +1,6 @@
 import {questions} from '/questions.js'
 
 
-
 let questionIndex = 0;
 let correctAnswers = 0;
 
@@ -12,11 +11,56 @@ const nextQuestion = document.getElementById('nxt');
 const question = document.getElementById('question')
 
 
-question.innerHTML = questions[questionIndex].question;
 
-questions[questionIndex].options.forEach(answer => {
-    const option = document.createElement("div")
-    option.innerHTML = answer.text
-    option.classList.add("option")
-    options.appendChild(option)
-});
+
+
+const renderQuestion= () => {
+    
+    options.innerHTML = '';
+    
+    const currentQuestion = questions[questionIndex]
+    question.innerHTML = currentQuestion.question;
+    nextQuestion.classList.add('inactive')
+
+    
+    if(currentQuestion.type !== "checkboxes"){
+        currentQuestion.options.forEach((answer) => {
+            const option = document.createElement("div");
+            option.innerHTML = answer.text;
+            option.classList.add("option");
+            
+            // Kollar om isCorrect är true eller false och lägger till en class beroende på vad som stämmer
+            option.addEventListener('click', () => {
+                if (answer.isCorrect) {
+                    correctAnswers++;
+                    option.classList.add("correct");
+                } else {
+                    option.classList.add("incorrect");
+                }
+                
+                // Disable all options after one is selected
+                nextQuestion.classList.remove('inactive')
+                Array.from(options.children).forEach(opt => opt.style.pointerEvents = 'none');
+            });
+            
+            options.appendChild(option);
+        });
+    }
+    }
+    
+    
+    const handleClick = () => {
+        questionIndex++;
+        renderQuestion()
+        console.log(correctAnswers)
+    }
+    
+    const renderCheckboxQuestions = () => {
+
+    }
+
+
+
+nextQuestion.addEventListener('click', handleClick);
+
+renderQuestion()
