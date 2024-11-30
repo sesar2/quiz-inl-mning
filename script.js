e41a7f7de8611d27736cb7bedfae1fae23d59839
sesar <54498315+sesar2@users.noreply.github.com>
2024-11-30 12:51:28 +0100

    js init

--- script.js
+++ script.js
@@ -1,5 +1,12 @@
 import {questions} from '/questions.js'
 
-console.log(questions)
+
 
 let questionIndex = 0;
+let correctAnswers = 0;
+
+
+const container = document.getElementById('quiz-window');
+const option = document.getElementById('option');
+const nextQuestion = document.getElementById('nxt');
+
