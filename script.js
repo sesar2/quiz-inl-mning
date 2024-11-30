a52f643f34c352c7cfc41351aac9e48e021eefd0
sesar <54498315+sesar2@users.noreply.github.com>
2024-11-30 13:03:51 +0100

    getting the data displayed on the screen correctly

--- script.js
+++ script.js
@@ -7,6 +7,16 @@ let correctAnswers = 0;
 
 
 const container = document.getElementById('quiz-window');
-const option = document.getElementById('option');
+const options = document.getElementById('options');
 const nextQuestion = document.getElementById('nxt');
+const question = document.getElementById('question')
 
+
+question.innerHTML = questions[questionIndex].question;
+
+questions[questionIndex].options.forEach(answer => {
+    const option = document.createElement("div")
+    option.innerHTML = answer.text
+    option.classList.add("option")
+    options.appendChild(option)
+});
